d5a02b9d696919a3427c09a0f25f76a66304acb7
dabamascodes <daniel.6@live.com.mx>
2025-02-19 14:22:25 -0700

    Cookies-Server Side

--- src/app/dashboard/cookies/page.tsx
+++ src/app/dashboard/cookies/page.tsx
@@ -1,3 +1,4 @@
+import { cookies } from 'next/headers';
 import { TabBar } from "@/components";
 
 
@@ -7,15 +8,25 @@ export const metadata = {
  description: 'SEO Title',
 };
 
-export default function CookiesPage() {
+export default async function CookiesPage() {
+
+  // const cookieStore = cookies();
+  // const cookieTab = cookieStore.get('selectedTab')?.value ?? '1';
+
+  const cookieStore = await cookies()
+  const cookieTab = cookieStore.get('selectedTab')?.value ?? '1';
+
+  // const allCookies = cookieStore.getAll();
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 gap-3">
+      {/* { JSON.stringify(allCookies)} */}
       
       <div>
         <span className="text-3xl">Tabs</span>
-        <TabBar />
+        <TabBar currentTab={ +cookieTab } />
       </div>
-      
+
     </div>
   );
 }
