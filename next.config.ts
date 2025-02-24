8684f3dcbff3fedefc2dc641afa1ffa0a4bd29bc
dabamascodes <daniel.6@live.com.mx>
2025-02-23 19:14:43 -0700

    Tarea - Mostrar información del usuario

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,21 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
+      // {
+      //   protocol: "https",
+      //   hostname: "tailus.io",
+      // },
       {
-        protocol: 'https',
-        hostname: 'tailus.io'
-      }
-    ]
-  }
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+    domains: ["avatars.githubusercontent.com"],
+  },
 };
 
 module.exports = nextConfig;
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
 import { CiLogout } from "react-icons/ci";
-import { SidebarItem } from "./SidebarItem";
 import { IoBasketOutline, IoCalendarOutline, IoCheckboxOutline, IoCodeWorkingOutline, IoListOutline } from "react-icons/io5";
+
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+
+import { SidebarItem } from "./SidebarItem";
+
 
 
 const menuItems = [
@@ -34,7 +39,17 @@ const menuItems = [
 
 ]
 
-export const Sidebar = () => {
+export const Sidebar = async() => {
+
+  const session = await getServerSession(authOptions);
+
+  const avatarUrl = ( session?.user?.image )
+    ? session.user.image
+    : 'https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png';
+  
+  const userName = session?.user?.name ?? 'No Name';
+  // TODO: const userRole = session?.user?.name ?? 'No Name';
+
   return (
     <aside className="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
       <div>
@@ -42,7 +57,7 @@ export const Sidebar = () => {
           {/* TODO: Next/Link hacia dashboard */}
           <Link href="#" title="home">
             {/* Next/Image */}
-            <Image src="https://tailus.io/sources/blocks/stats-cards/preview/images/logo.svg"
+            <Image src="https://upload.wikimedia.org/wikipedia/commons/5/52/Free_logo.svg"
               className="w-32"
               alt="tailus logo"
               width={150}
@@ -54,14 +69,14 @@ export const Sidebar = () => {
         <div className="mt-8 text-center">
           {/* Next/Image */}
           <Image
-            src="https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp"
+            src={ avatarUrl }
             width={150}
             height={150}
             alt=""
             className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28"
           />
           <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">
-            Cynthia J. Watts
+            { userName }
           </h5>
           <span className="hidden text-gray-400 lg:block">Admin</span>
         </div>
