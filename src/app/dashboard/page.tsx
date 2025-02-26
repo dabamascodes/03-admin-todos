acdff7823f924f155d30ae1e56da2c96a39b7572
dabamascodes <daniel.6@live.com.mx>
2025-02-26 15:46:34 -0700

    UUID en lugar CUID

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -13,7 +13,7 @@ export default async function DashboardPage() {
   }
 
   return (
-    <div className="grid gap-6 grid-cols-1 sm:grid-cols-2">
+    <div className="grid gap-6 grid-cols-1">
       {/* TODO: src/components <WidgetItem /> */}
       <WidgetItem title="Usuario conectado S-Side">
         {/* {
@@ -23,6 +23,10 @@ export default async function DashboardPage() {
           <span>{ session.user?.name }</span>
           <span>{ session.user?.image }</span>
           <span>{ session.user?.email }</span>
+
+          <div>
+            { JSON.stringify( session ) }
+          </div>
         </div>
       </WidgetItem>
       {/* TODO: Fin <WidgetItem /> */}
