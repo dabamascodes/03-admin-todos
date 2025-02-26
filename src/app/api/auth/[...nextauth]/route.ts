be0a80e8807f622319c817e16b689391a4f21366
dabamascodes <daniel.6@live.com.mx>
2025-02-26 16:39:29 -0700

    Campos adicionales al usuario

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -22,6 +22,39 @@ export const authOptions:NextAuthOptions = {
     }),
 
   ],
+
+  session: {
+    strategy: 'jwt'
+  },
+
+  callbacks: {
+
+    async signIn({ user, account, profile, email, credentials }) {
+      console.log({user});
+      return true;
+    },
+
+    async jwt({ token, user, account, profile }) {
+      // console.log({token});
+      const dbUser = await prisma.user.findUnique({ where: { email: token.email ?? 'no-email' } });
+
+      token.roles = dbUser?.roles ?? ['no-roles']
+      token.id = dbUser?.id ?? ['no-uuid']
+
+      return token;
+    },
+
+    async session({ session, token, user }) {
+      // console.log({token});
+
+      if ( session && session.user ) {
+        session.user.roles = token.roles;
+        session.user.id = token.id;
+      }
+
+      return session;
+    }
+  }
 }
 
 const handler = NextAuth(authOptions);
