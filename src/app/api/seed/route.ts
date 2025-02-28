19da528841878f15f7b982083bacffd4d2258395
dabamascodes <daniel.6@live.com.mx>
2025-02-28 15:14:33 -0700

    Relaciones y semilla de bases de datos

--- src/app/api/seed/route.ts
+++ src/app/api/seed/route.ts
@@ -1,27 +1,44 @@
-import prisma from '@/lib/prisma';
-import { NextResponse, NextRequest } from 'next/server'
+import prisma from "@/lib/prisma";
+import bcrypt from "bcryptjs";
+import { NextResponse, NextRequest } from "next/server";
 
-export async function GET(request: Request) { 
+export async function GET(request: Request) {
+  // await prisma.todo.deleteMany(); // delete * from todo
 
-    await prisma.todo.deleteMany(); // delete * from todo
+  // // const todo = await prisma.todo.create({
+  // //     data: { description: 'Piedra del alma', complete: true },
+  // // });
+  // // console.log(todo);
 
-    // const todo = await prisma.todo.create({
-    //     data: { description: 'Piedra del alma', complete: true },
-    // });
-    // console.log(todo);
+  // await prisma.todo.createMany({
+  //     data:[
+  //         { description: 'Piedra del alma', complete: true },
+  //         { description: 'Piedra del poder' },
+  //         { description: 'Piedra del tiempo' },
+  //         { description: 'Piedra del espacio' },
+  //         { description: 'Piedra del realidad' },
+  //     ]
+  // })
 
-    await prisma.todo.createMany({
-        data:[
-            { description: 'Piedra del alma', complete: true },
-            { description: 'Piedra del poder' },
-            { description: 'Piedra del tiempo' },
-            { description: 'Piedra del espacio' },
-            { description: 'Piedra del realidad' },
-        ]
-    })
+  await prisma.todo.deleteMany(); // delete * from todo
+  await prisma.user.deleteMany(); // delete * from user
 
+  const user = await prisma.user.create({
+    data: {
+      email: "test1@google.com",
+      password: bcrypt.hashSync("123456"),
+      roles: ["admin", "client", "super-user"],
+      todos: {
+        create: [
+          { description: "Piedra del alma", complete: true },
+          { description: "Piedra del poder" },
+          { description: "Piedra del tiempo" },
+          { description: "Piedra del espacio" },
+          { description: "Piedra del realidad" },
+        ],
+      },
+    },
+  });
 
-
-
-    return NextResponse.json({ message: 'Seed Executed' });
+  return NextResponse.json({ message: "Seed Executed" });
 }
